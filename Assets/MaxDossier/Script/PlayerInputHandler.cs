b1276c01b87b9784d8231ead5c57aeeed8f7545e
MaximeBritto <maxime.britto@hotmail.com>
2022-04-28 14:49:03 +0200

    2 joueur pour lancer la game

--- Assets/MaxDossier/Script/PlayerInputHandler.cs
+++ Assets/MaxDossier/Script/PlayerInputHandler.cs
@@ -7,7 +7,7 @@ public class PlayerInputHandler : MonoBehaviour
 {
     playerzer player;
     private static Player players;
-    private static int playerCount = 0;
+    public static int playerCount = 0;
 
     [SerializeField] List<GameObject> prefabs = new List<GameObject>();
 
